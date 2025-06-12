d2945a183ea32d4456081c3e198f7d2c20ce272c
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-06-12 15:48:14 -0600

    feat: agregar dao, modelo y conexión a la base de datos

---
+++ app/src/main/java/com/hadoga/data/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.hadoga.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.hadoga.data.dao.UserDao;
+import com.hadoga.data.model.User;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+
+    public abstract UserDao userDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "dam_database")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instance;
+    }
+}
