6c57db6ae0dbef93e171e06a84cbca862dad5923
gabo <cc22060@ues.edu.sv>
2025-06-12 20:13:07 -0600

    feat: agregar funcionalidad al botón de ingresar

--- app/src/main/java/com/hadoga/dam_project/LoginActivity.java
+++ app/src/main/java/com/hadoga/dam_project/LoginActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +16,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.hadoga.data.AppDatabase;
+import com.hadoga.data.model.User;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+    Button btnIngresar, btnSalir;
+    AppDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        // Conectar vistas
+        editTextEmail = findViewById(R.id.textview_usuario);
+        editTextPassword = findViewById(R.id.edittext_password);
+        btnIngresar = findViewById(R.id.btn_ingresar);
+        btnSalir = findViewById(R.id.btn_salir);
+
+        // Inicializar base de datos
+        db = AppDatabase.getInstance(this);
+        btnIngresar.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+
+            if (email.isEmpty()) {
+                editTextEmail.setError("Campo obligatorio");
+                return;
+            }
+
+            if (password.isEmpty()) {
+                editTextPassword.setError("Campo obligatorio");
+                return;
+            }
+
+            User user = db.userDao().login(email, password);
+            if (user != null) {
+                Toast.makeText(this, "¡Bienvenido " + user.username + "!", Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSalir.setOnClickListener(v -> finish());
+
 
         // Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
