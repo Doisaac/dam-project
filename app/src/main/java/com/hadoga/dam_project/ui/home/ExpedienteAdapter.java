6f51f33385099380671bab6c28276739a710607c
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-06-17 21:57:36 -0600

    feat: cargar imagen en lista de expedientes

--- app/src/main/java/com/hadoga/dam_project/ui/home/ExpedienteAdapter.java
+++ app/src/main/java/com/hadoga/dam_project/ui/home/ExpedienteAdapter.java
@@ -1,6 +1,8 @@
 package com.hadoga.dam_project.ui.home;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +15,14 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hadoga.dam_project.R;
 import com.hadoga.data.AppDatabase;
 import com.hadoga.data.model.Expediente;
 
+import java.io.File;
 import java.util.List;
 
 public class ExpedienteAdapter extends RecyclerView.Adapter<ExpedienteAdapter.ViewHolder> {
@@ -43,6 +47,18 @@ public class ExpedienteAdapter extends RecyclerView.Adapter<ExpedienteAdapter.Vi
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Expediente expediente = lista.get(position);
+
+        // Cargar foto
+        if (expediente.fotoUri != null && !expediente.fotoUri.isEmpty()) {
+            File file = new File(Uri.parse(expediente.fotoUri).getPath());
+            if (file.exists()) {
+                holder.imageExpediente.setImageURI(Uri.parse(expediente.fotoUri));
+            } else {
+                holder.imageExpediente.setImageResource(R.drawable.ic_person_placeholder);
+            }
+        } else {
+            holder.imageExpediente.setImageResource(R.drawable.ic_person_placeholder);
+        }
 
         holder.textNombre.setText(expediente.nombre + " " + expediente.apellido);
         holder.textNacimiento.setText("Nacimiento: " + expediente.fechaNacimiento);
@@ -79,11 +95,13 @@ public class ExpedienteAdapter extends RecyclerView.Adapter<ExpedienteAdapter.Vi
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageExpediente;
         TextView textNombre, textNacimiento;
         Button btnEditar, btnEliminar;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            imageExpediente = itemView.findViewById(R.id.imageExpediente);
             textNombre = itemView.findViewById(R.id.textNombre);
             textNacimiento = itemView.findViewById(R.id.textNacimiento);
             btnEditar = itemView.findViewById(R.id.btnEditar);
